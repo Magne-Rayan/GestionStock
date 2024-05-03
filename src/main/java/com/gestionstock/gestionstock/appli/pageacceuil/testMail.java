290f1e2eb986cfbb84377d5da0b6ddd56c745347
Magne Rayan <r.magne@lprs.fr>
2024-05-03 10:30:34 +0200

    fix : test class mail

---
+++ src/main/java/com/gestionstock/gestionstock/appli/pageacceuil/testMail.java
@@ -0,0 +1,15 @@
+package com.gestionstock.gestionstock.appli.pageacceuil;
+
+import com.gestionstock.gestionstock.appli.pagemecano.BonDebit;
+import com.gestionstock.gestionstock.entity.Mail;
+import com.itextpdf.text.Document;
+
+public class testMail {
+    public static void main(String[] args) {
+
+        Mail mail = new Mail();
+
+mail.jakartaEmail();
+
+    }
+    }
